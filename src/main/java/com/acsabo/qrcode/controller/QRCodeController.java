f52d6ec7d9d4e36afe2ff6821d4ec0adc32fac8f
Adriano Sabo <adriano.sabo@ciandt.com>
2025-08-05 15:41:04 -0300

    Initial version

---
+++ src/main/java/com/acsabo/qrcode/controller/QRCodeController.java
@@ -0,0 +1,9 @@
+package com.acsabo.qrcode.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class QRCodeController {
+
+
+}
---
+++ src/test/java/com/acsabo/qrcode/QrcodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acsabo.qrcode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QrcodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
