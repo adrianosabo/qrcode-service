63fc45b283e365667b1a65ad752011c3c960e526
Adriano Sabo <adriano.sabo@ciandt.com>
2025-08-05 15:59:00 -0300

    comments

--- src/main/java/com/acsabo/qrcode/controller/QRCodeController.java
+++ src/main/java/com/acsabo/qrcode/controller/QRCodeController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 
 @Controller
 public class QRCodeController {
-
+	// This class will handle the QR code generation and display logic
+	// You can add methods here to generate QR codes, handle requests, etc.
 
 }
