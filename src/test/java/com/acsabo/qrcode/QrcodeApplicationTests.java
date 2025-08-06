ad841a15ca8ee09729c2409ec2269c482ebb1cea
Adriano Sabo <adriano.sabo@ciandt.com>
2025-08-06 12:03:50 -0300

    test

--- src/test/java/com/acsabo/qrcode/QrcodeApplicationTests.java
+++ src/test/java/com/acsabo/qrcode/QrcodeApplicationTests.java
@@ -1,13 +1,28 @@
-package com.acsabo.qrcode;
+package com.acsabo.qrcode.controller;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
-class QrcodeApplicationTests {
+@AutoConfigureMockMvc
+class QRCodeControllerTest {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    private MockMvc mockMvc;
 
+    @Test
+    void testGenerateQRCode() throws Exception {
+        mockMvc.perform(get("/qrcode")
+                .param("text", "HelloWorld"))
+                .andExpect(status().isOk())
+                .andExpect(header().string("Content-Type", MediaType.IMAGE_PNG_VALUE))
+                .andExpect(content().contentType(MediaType.IMAGE_PNG));
+    }
 }
